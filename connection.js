d5f008545eb8dde33ae242d6d7274c91e777916b
Toni_Fields <57774418+NoSoyTuMadre@users.noreply.github.com>
2020-11-04 20:48:24 -0500

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,4 +1,4 @@
-fetch("https://databaseid-region.apps.astra.datastax.com/api/rest/v1/auth", {
+fetch("https://a4ac4fa4-adfa-46fe-a28b-d9522c5e475c-us-east1.apps.astra.datastax.com/api/rest/v1/auth", {
   "method": "POST",
   "headers": {
     "Accept": "*/*",

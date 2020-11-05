da28a4cda8b9efceccbdcc87d246682bc4137aeb
Toni_Fields <57774418+NoSoyTuMadre@users.noreply.github.com>
2020-11-04 21:04:10 -0500

    Create add_users.js

---
+++ add_users.js
@@ -0,0 +1,13 @@
+fetch("https://a4ac4fa4-adfa-46fe-a28b-d9522c5e475c-us-east1.apps.astra.datastax.com/api/rest/v1/keyspaces/kids_space/tables/users/rows", {
+  "method": "POST",
+  "headers": {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+  }
+})
+.then(response => {
+  console.log(response);
+})
+.catch(err => {
+  console.error(err);
+});
